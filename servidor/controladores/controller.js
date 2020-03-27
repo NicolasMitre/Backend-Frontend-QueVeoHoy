3f88f80eb16ce915a8c40697bb006fc4682c42cd
NicolasMitre <nicolasmelluso7@gmail.com>
2020-03-27 10:18:00 -0300

    fix the sql query anti scripting

--- servidor/controladores/controller.js
+++ servidor/controladores/controller.js
@@ -20,33 +20,29 @@ const getPeliculas = (req, res) => {
   const tamanioPag = parseInt(req.query.cantidad) || 1;
   const nroPagina = parseInt(req.query.pagina) || 1;
   const offset = tamanioPag * (nroPagina - 1);
-  const filtro = `
-        ${filtroAnio}   AND
-        ${filtroTitulo} AND
-        ${filtroGenero}        
-    `;
+  const filtro = `${filtroAnio} AND ${filtroTitulo} AND ${filtroGenero}`;
   const sql = `
         select p.* 
         from pelicula as p
         inner join genero as g on g.id = p.genero_id
-        where ${filtro}   
-        ${orderBy} 
-        limit ${tamanioPag}
-        offset ${offset}
+        where ?
+        ? 
+        limit ?
+        offset ?
         `;
-  conn.query(sql, (err, result) => {
+
+  conn.query(sql, [filtro, orderBy, tamanioPag, offset], (err, result) => {
     if (err) {
-      console.log("1", err);
+      console.log(err);
       return res.status(500).send("ERROR");
     }
-
     const resultado = {
       peliculas: result,
       total: 0
     };
 
-    const sqlTotal = `select count(*) as total from pelicula as p where ${filtro}`;
-    conn.query(sqlTotal, (err, result) => {
+    const sqlTotal = `select count(*) as total from pelicula as p where ?`;
+    conn.query(sqlTotal, [filtro], (err, result) => {
       if (err) {
         console.log("1", err);
         return res.status(500).send("ERROR");
@@ -74,8 +70,8 @@ const getPeliculaId = (req, res) => {
     select p.*, g.nombre
         from pelicula p 
         inner join genero g on g.id = p.genero_id
-        where p.id = ${id}`;
-  conn.query(sql, (err, result) => {
+        where p.id = ?`;
+  conn.query(sql, [id], (err, result) => {
     if (err) return res.status(500).send("ERROR");
     if (result.length === 0) return res.status(500).send("ERROR");
     const sql2 = `
@@ -83,8 +79,8 @@ const getPeliculaId = (req, res) => {
             from actor as a
             inner join actor_pelicula ap on ap.actor_id = a.id
             inner join pelicula as p on p.id = ap.pelicula_id
-            where p.id = ${id}`;
-    conn.query(sql2, (err, result2) => {
+            where p.id = ?`;
+    conn.query(sql2, [id], (err, result2) => {
       if (err) return res.status(500).send("ERROR");
       res.json({
         pelicula: result[0],
@@ -112,19 +108,22 @@ const getRecomendacion = (req, res) => {
     from pelicula  as p 
     inner join genero g on g.id = p.genero_id
     where 1=1
-    
-    ${filtroGenero}
-    ${filtroAnioInicio}
-    ${filtroAnioFin}
-    ${filtroPuntuacion}
+    ?
+    ?
+    ?
+    ?
      `;
-  conn.query(sql, (err, result) => {
-    console.log(err);
-    if (err) return res.status(500).send("Error");
-    res.json({
-      peliculas: result
-    });
-  });
+  conn.query(
+    sql,
+    [filtroGenero, filtroAnioInicio, filtroAnioFin, filtroPuntuacion],
+    (err, result) => {
+      console.log(err);
+      if (err) return res.status(500).send("Error");
+      res.json({
+        peliculas: result
+      });
+    }
+  );
 };
 
 module.exports = {
